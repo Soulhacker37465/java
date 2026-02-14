87a8057dd6de8555eeec76ec7edab28f28ffe708
Soulhacker37465 <teachnicalgaurav45@gmail.com>
2026-02-14 19:18:38 +0530

    code with java

---
+++ CWG_12PS2.java
@@ -0,0 +1,22 @@
+public class CWG_12PS2 {
+    public static void main(String[] args) {
+        /*char grade = 'B';
+
+        // Encrypt
+        grade = (char)(grade + 6);
+        System.out.println(grade); */
+
+        /*Scanner sc =  new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a>8);*/
+
+        /*int a = 2; int v = 8; int u = 4;  int s = 4;
+        int time = ((v*v) - (u*u))/(2*a*s);
+
+        System.out.println(time);*/
+
+        int a = 7 * 49/7 + 35/7;
+        System.out.println(a);
+
+    }
+}
