279b689827a6836466225a18b5fcc7559dfff3c6
Soulhacker37465 <teachnicalgaurav45@gmail.com>
2026-02-14 19:51:45 +0530

    code with java

---
+++ CWG_13string.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CWG_13string {
+    public static void main(String[] args) {
+        // String name =  new String("Gaurav");
+        String name = "Gaurav";
+        System.out.print("The name is : ");
+        System.out.println(name);
+
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println(st);
+    }
+}
---
+++ CWG_14String_Method.java
@@ -0,0 +1,14 @@
+public class CWG_14String_Method {
+    public static void main(String[] args) {
+        String name = "gaurav";
+        System.out.println(name.length());
+        // System.out.println(name.toLowerCase());
+        // System.out.println(name.toUpperCase());
+        // String nonTrimmString = "    Gaurav     ";
+        // System.out.println(nonTrimmString);
+        // System.out.println(nonTrimmString.trim());
+
+        System.out.println(name.substring(2));
+
+    }
+}
