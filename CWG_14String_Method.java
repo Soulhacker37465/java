f2c0c6674c0c104fa9aabcf875178f344c0cb883
Gaurav <teachnicalgaurav45@gmail.com>
2026-02-18 02:11:41 +0530

    Java with Gaurav

--- CWG_14String_Method.java
+++ CWG_14String_Method.java
@@ -8,7 +8,13 @@ public class CWG_14String_Method {
         // System.out.println(nonTrimmString);
         // System.out.println(nonTrimmString.trim());
 
-        System.out.println(name.substring(2));
-
+        // System.out.println(name.substring(2)); // 2 se start hoga
+        // System.out.println(name.substring(1,4)); // 1 se start hoga but not 4 tak 3 tak hoga
+        // System.out.println(name.replace("u", "n"));
+        // System.out.println(name.replace("rav", "tham"));
+        // System.out.println(name.charAt(2));
+        // System.out.println(name.indexOf("ra"));
+        System.out.println(name.lastIndexOf("ras"));
+        System.out.println(name.equalsIgnoreCase("Gaurav"));
     }
 }
---
+++ CWG_15java_practice.java
@@ -0,0 +1,11 @@
+public class CWG_15java_practice {
+    public static void main(String[] args) {
+        // String n = "G aurav";
+        // System.out.println(n.replace(" ", "_"));
+        // String Letter = "Dear <|name|> ,You are a trans a bit";
+        // System.out.println(Letter.replace("<|name|>", "Saurabh"));
+
+        String myLetter = "Dear Gaurav,\n\tThis java course is so nice.\n\tThanks!";
+        System.out.println(myLetter);
+    }
+}
